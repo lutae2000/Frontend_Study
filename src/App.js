a3f59fa785d45350e66eb38c50f3b434b92677e8
lutae2000 <lutae2000@gmail.com>
2022-01-07 02:15:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [],
+      items: [
+        { id: "0", title: "hello world 1", done: true },
+        { id: "1", title: "hello world 2", done: false },
+      ],
     };
   }
 
