2ec5d5be0fd843d11b4a595e23c85aa132c79625
lutae2000 <lutae2000@gmail.com>
2022-01-10 01:56:18 +0900

    Update App.js
    update 부분 구현

--- src/App.js
+++ src/App.js
@@ -82,7 +82,12 @@ class App extends React.Component {
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} />
+            <Todo
+              item={item}
+              key={item.id}
+              delete={this.delete}
+              update={this.update}
+            />
           ))}
         </List>
       </Paper>
